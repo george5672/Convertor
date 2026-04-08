5374cd61def4bcffde917465b326b004eea2cb6c
Sanya550 <pivovarsasa9@gmail.com>
2026-04-08 19:22:05 +0300

    Added System.out Version 1

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -7,22 +7,20 @@ public class Main {
         System.out.println("Converter App.");
 
         double kgs = 5;
-        double pnds=12;
-        double pounds = convKgsToPounds (kgs);
-        double  kilos= convKgsToPounds (pnds);
-        System.out.println("Result is" + pounds + " pounds and" +  kilos +"kgs.");
+        double pnds = 12;
+        double pounds = convKgsToPounds(kgs);
+        double kilos = convPoundsToKgs(pnds);
+        System.out.println("Result is" + pounds + " pounds and" + kilos + "kgs.");
         System.out.println("Version 1.0.");
     }
+
     private static double convKgsToPounds(double kgs) {
         return kgs * CONV_K;
     }
 
-    private static double convToPoundsKgs(double pnds) {
-        return pnds/CONV_K;
+    private static double convPoundsToKgs(double pnds) {
+        return pnds / CONV_K;
     }
-
-
-
 
 
 }
