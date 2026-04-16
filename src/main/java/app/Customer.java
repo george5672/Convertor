13fdcfeb84ae4a23509ab30898702c2b06e9eeef
george5672 <zorapivovar@gmail.com>
2026-04-16 23:26:19 +0300

    Customer.java

--- src/main/java/app/Customer.java
+++ src/main/java/app/Customer.java
@@ -17,8 +17,21 @@ private final String name;
         return name;
     }
 
-    public String getPhone() {
-        return phone;
+    public static void main(String[] args) {
+        Customer customer = getCustomer(getData());
+        String output = "Customer: " + customer.getName() + ", phone " + customer.getName();
+        getOutput(output);
+    }
 
-}
+    public static String[] getData() {
+        return new String[]{"Жора", "+380000000000"};
+    }
+
+    public static Customer getCustomer(String[] data) {
+        return new Customer(data[0], data[1]);
+    }
+
+    public static void getOutput(String output) {
+        System.out.println(output);
+    }
 }
