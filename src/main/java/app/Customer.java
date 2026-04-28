00c7975878e476194d1c5d5dbbb998eae6669ce3
george5672 <zorapivovar@gmail.com>
2026-04-28 20:15:58 +0300

    Customer.java

--- src/main/java/app/Customer.java
+++ src/main/java/app/Customer.java
@@ -1,8 +1,8 @@
+
 package app;
 
 public class Customer {
-
-private final String name;
+    private final String name;
     private final String phone;
 
     public Customer(String name, String phone) {
@@ -10,26 +10,11 @@ private final String name;
         this.phone = phone;
     }
 
-   
     public String getName() {
         return name;
     }
 
-    public static void main(String[] args) {
-        Customer customer = getCustomer(getData());
-        String output = "Customer: " + customer.getName() + ", phone " + customer.getName();
-        getOutput(output);
+    public String getPhone() {
+        return phone;
     }
 
-    public static String[] getData() {
-        return new String[]{"Жора", "+380000000000"};
-    }
-
-    public static Customer getCustomer(String[] data) {
-        return new Customer(data[0], data[1]);
-    }
-
-    public static void getOutput(String output) {
-        System.out.println(output);
-    }
-}
