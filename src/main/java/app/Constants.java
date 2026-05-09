7b6c87760fdbd339281722d91a5ace232cb2b72e
Sanya550 <pivovarsasa9@gmail.com>
2026-05-09 21:45:22 +0300

    Added  Constants

---
+++ src/main/java/app/Constants.java
@@ -0,0 +1,12 @@
+package app;
+
+public final class Constants {
+
+    public final static String CURRENCY = "EUR";
+    public final static String MEASURE = "pcs.";
+}
+
+
+
+
+
--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -3,20 +3,31 @@ package app;
 public class Main {
 
     public static void main(String[] args) {
-        Customer customer = getCustomer(getData());
-
-        getOutput("Customer: %s, phone: %s".formatted(
-                customer.getName(),
-                customer.getPhone()
-        ));
+        // Отримуємо вхідні дані
+        String[] data = getData();
+        // Формуємо об'єкт з вхідних даних
+        Product product = new Product(data[0],
+                Integer.parseInt(data[1]),
+                Double.parseDouble(data[2]));
+        // Розраховуємо базову вартість товару
+        CalcCostBase costBase = new CalcCostBase();
+        double baseCost = costBase.calcCost(product);
+        // Розраховуємо вартість товару, з урахуванням доставки
+        CalcCostDelivery costDelivery = new CalcCostDelivery();
+        double deliveryCost = costDelivery.calcCost(product);
+        // Формуємо виведення
+        String baseOutput = product + "\nCost is " +
+                Constants.CURRENCY + " " + baseCost + ".";
+        String deliveryOutput = product + "\nCost is " +
+                Constants.CURRENCY + " " + deliveryCost + ".";
+        // Виводимо результат
+        getOutput(baseOutput);
+        getOutput(deliveryOutput);
     }
 
+    // Набір вхідних даних
     public static String[] getData() {
-        return new String[]{"Tom", "555 123-8596"};
-    }
-
-    public static Customer getCustomer(String[] data) {
-        return new Customer(data[0], data[1]);
+        return new String[] {"abc", "5", "2.5"};
     }
 
     public static void getOutput(String output) {
