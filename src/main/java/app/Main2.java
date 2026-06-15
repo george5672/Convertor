f49b8e54d3eead03e4f1c0f33810e8a03f582c31
Sanya550 <pivovarsasa9@gmail.com>
2026-06-15 21:00:04 +0300

    Added Main2

--- src/main/java/app/Main2.java
+++ src/main/java/app/Main2.java
@@ -1,14 +1,24 @@
 public class Main2 {
     public static void main(String[] args) {
+
         System.out.println("Проект: Converter (Версія 2.0)");
-        System.out.println("Автор: george5672");
+        System.out.println("Автор: george5672\n");
+
 
         double miles = 5.0;
         System.out.println(miles + " миль = " + milesTOKm(miles) + " км");
+
+
+        double kilometers = 10.0;
+        System.out.println(kilometers + " км = " + kmTOMiles(kilometers) + " миль");
     }
 
-    // Додаємо сам метод конвертації (Пункт 8)
+
     public static double milesTOKm(double miles) {
         return miles * 1.60934;
+    }
+
+    public static double kmTOMiles(double kilometers) {
+        return kilometers / 1.60934;
     }
 }
